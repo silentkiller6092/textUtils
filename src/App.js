5e300b1b56ef9837a25d3eab4f5507332142449c
Shivam Pandey <79106500+silentkiller6092@users.noreply.github.com>
2023-09-12 12:40:40 +0530

    Added the Routing components in App.js
    In this commit, routing components were integrated into App.js, allowing seamless page navigation in your React web application without requiring full page reloads. Key points include importing and using react-router-dom components for routing, setting up the router with <Router>, configuring routes with <Routes>, enabling page navigation through links, and ensuring a smoother user experience with client-side routing.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import About from "./components/About";
 import NavBar from "./components/NavBar";
 import TextForm from "./components/TextForm";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   const [darkmode, setdarkmode] = useState("light");
@@ -10,19 +11,29 @@ function App() {
     if (darkmode == "light") {
       setdarkmode("dark");
       document.body.style.backgroundColor = "#042743";
+      document.title = "TextUtils Dark Mode";
     } else {
       setdarkmode("light");
       document.body.style.backgroundColor = "white";
+      document.title = "TextUtils Light Mode";
     }
   };
   return (
-    <div>
+    <Router>
       <NavBar title="TextUtils" mode={darkmode} toogleMode={toggleMode} />
       <div className="container">
-        <TextForm heading="Enter the text to analyze" mode={darkmode} />
+        <Routes>
+          <Route exact path="/about" element={<About />} />
+          <Route
+            exact
+            path="/"
+            element={
+              <TextForm heading="Enter the text to analyze" mode={darkmode} />
+            }
+          />
+        </Routes>
       </div>
-      {/* <About /> */}
-    </div>
+    </Router>
   );
 }
 
